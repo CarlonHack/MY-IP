ed35df53e45c88f7890c8e9442eff38de4134f6a
CARLON <99050212+CarlonHack@users.noreply.github.com>
2023-08-23 20:53:09 +0300

    Add files via upload

---
+++ if.sh
@@ -0,0 +1,8 @@
+echo '''                         MY-IP
+                              Developer By Carlon
+                              github:github.com/CarlonHack/'''
+                
+echo "IP ADRESIN"
+ifconfig
+                              
+              
